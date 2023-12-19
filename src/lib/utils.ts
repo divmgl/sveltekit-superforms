7d5f55d659f1b45b042735e5a2e1f3f60c2d9d08
Dexter Miguel <5452298+divmgl@users.noreply.github.com>
2023-12-18 19:53:18 -0500

    :wrench:

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { parse, stringify } from 'devalue';
+import { klona } from 'klona';
 
 // Thanks to: https://dev.to/tylim88/typescript-numeric-range-type-15a5#comment-22mld
 export type NumericRange<
@@ -16,5 +16,5 @@ export type NumericRange<
     >;
 
 export function clone<T>(data: T): T {
-  return parse(stringify(data));
+  return klona(data);
 }
